ca1c185e71adbb8de77ca6699075527cb644a0ea
Ansh <anshinbox44@gmail.com>
2023-07-19 15:29:15 +0530

    Strings

--- t7.py
+++ t7.py
@@ -1,6 +1,6 @@
 # Turtle program of making a flower :----
 
-import turtle as tur
+"""import turtle as tur
 import colorsys as cs 
 tur.setup(800,800)
 tur.speed(0)
@@ -17,4 +17,68 @@ for j in range(25):
         tur.right(180)
         tur.circle(50,24)
         tur.hideturtle()
-        tur.done()
+        tur.done()"""
+
+
+# Python program to draw square
+# using Turtle Programming
+
+"""import turtle
+skk = turtle.Turtle()
+
+for i in range(4):
+	skk.forward(50)
+	skk.right(90)
+	
+turtle.done()"""
+
+# Python program to draw star
+# using Turtle Programming
+
+"""import turtle
+star = turtle.Turtle()
+
+star.right(75)
+star.forward(100)
+
+for i in range(4):
+	star.right(144)
+	star.forward(100)
+	
+turtle.done()"""
+
+# Python program to draw hexagon
+# using Turtle Programming
+
+"""import turtle
+polygon = turtle.Turtle()
+
+num_sides = 6
+side_length = 70
+angle = 360.0 / num_sides
+
+for i in range(num_sides):
+	polygon.forward(side_length)
+	polygon.right(angle)
+	
+turtle.done()"""
+
+# Python program to draw
+# Spiral Helix Pattern
+# using Turtle Programming
+
+"""import turtle
+loadWindow = turtle.Screen()
+turtle.speed(2)
+
+for i in range(100):
+	turtle.circle(5*i)
+	turtle.circle(-5*i)
+	turtle.left(i)
+
+turtle.exitonclick()"""
+
+
+
+
+
